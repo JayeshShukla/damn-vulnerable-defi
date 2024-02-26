08373d9b17ab0f184dc0d3eae775bdd6e6708622
Jayesh Shukla <jayeshshukla17@gmail.com>
2024-02-26 15:00:31 +0530

    - solved unstoppable

--- contracts/unstoppable/ReceiverUnstoppable.sol
+++ contracts/unstoppable/ReceiverUnstoppable.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
 import "solmate/src/auth/Owned.sol";
-import { UnstoppableVault, ERC20 } from "../unstoppable/UnstoppableVault.sol";
+import {UnstoppableVault, ERC20} from "../unstoppable/UnstoppableVault.sol";
 
 /**
  * @title ReceiverUnstoppable
@@ -18,6 +18,14 @@ contract ReceiverUnstoppable is Owned, IERC3156FlashBorrower {
         pool = UnstoppableVault(poolAddress);
     }
 
+    /**
+     * @dev
+     * 1) initiator needs to be "this"
+     * 2) caller has to be #UNS
+     * 3) token needs to be #DVT
+     * 4) fee should be 0
+     * 5) token.allowance(initiator, pool(#UNS)) -> amount
+     */
     function onFlashLoan(
         address initiator,
         address token,
@@ -25,21 +33,27 @@ contract ReceiverUnstoppable is Owned, IERC3156FlashBorrower {
         uint256 fee,
         bytes calldata
     ) external returns (bytes32) {
-        if (initiator != address(this) || msg.sender != address(pool) || token != address(pool.asset()) || fee != 0)
-            revert UnexpectedFlashLoan();
-
+        if (
+            initiator != address(this) ||
+            msg.sender != address(pool) ||
+            token != address(pool.asset()) ||
+            fee != 0
+        ) revert UnexpectedFlashLoan();
+        /**
+         *
+         * @dev #REC allows #UNS to spend amount & adds in #DVT.
+         */
         ERC20(token).approve(address(pool), amount);
 
         return keccak256("IERC3156FlashBorrower.onFlashLoan");
     }
 
+    /**
+     * @dev creates local asset = #DVT.
+     * executes flashLoan of #UNS(pool)
+     */
     function executeFlashLoan(uint256 amount) external onlyOwner {
         address asset = address(pool.asset());
-        pool.flashLoan(
-            this,
-            asset,
-            amount,
-            bytes("")
-        );
+        pool.flashLoan(this, asset, amount, bytes(""));
     }
 }
